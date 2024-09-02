3ee660c44fdfd00961c51f4c96ff2a0f06b6ccc1
Jonathan Bae <jonbae.mingun@gmail.com>
2024-09-02 14:12:40 -0400

    reset ball

--- src/main.zig
+++ src/main.zig
@@ -13,22 +13,34 @@ pub fn main() !void {
     var ball = b.Ball.init(50, 50, 32, 4);
     var p1paddle = p.Paddle.init(0, 0, p.Paddle.PADDLE_WIDTH, p.Paddle.PADDLE_HEIGHT, 4);
     var p2paddle = p.Paddle.init(screen.SCREEN_WIDTH - p.Paddle.PADDLE_WIDTH, 0, p.Paddle.PADDLE_WIDTH, p.Paddle.PADDLE_HEIGHT, 4);
+
     while (!r.WindowShouldClose()) {
         r.BeginDrawing();
         defer r.EndDrawing();
         r.ClearBackground(r.BLACK);
 
+        // r.DrawText(r.TextFormat("Elapsed Time: %4.3f ms", r.GetFrameTime() * 1000), 200, 220, 20, r.WHITE);
+        // std.debug.print("frame time: {}\n time elapsed: {}\n", .{ r.GetFrameTime() * 1000, r.GetTime() * 1000 });
+
         // UPDATE
         ball.update();
         p1paddle.update_movement(r.KEY_W, r.KEY_S);
         p2paddle.update_movement(r.KEY_UP, r.KEY_DOWN);
-        ball.wall_collision_detection();
-        ball.paddle_collision_detection(&p1paddle);
-        ball.paddle_collision_detection(&p2paddle);
+        if (ball.tb_wall_collision_detection()) {
+            ball.speed.y *= -1;
+        }
+        if (ball.paddle_collision_detection(&p1paddle) or ball.paddle_collision_detection(&p2paddle)) {
+            ball.speed.x *= -1;
+        }
 
         // DRAW
-        ball.draw();
+        if (!ball.lr_wall_collision_dectection()) {
+            ball.draw();
+        } else {
+            ball.reset();
+        }
         p1paddle.draw();
         p2paddle.draw();
     }
+    r.EndDrawing();
 }
--- src/structs/Ball.zig
+++ src/structs/Ball.zig
@@ -15,25 +15,25 @@ pub const Ball = struct {
         self.position = r.Vector2Add(self.position, self.speed);
     }
 
-    pub fn wall_collision_detection(self: *Ball) void {
-        //check for y collisions
-        if (self.position.y <= 0 or (self.position.y + self.size.y) > screen.SCREEN_HEIGHT) {
-            self.speed.y *= -1;
-        }
-
-        // check for x collisions
-        if (self.position.x <= 0 or (self.position.x + self.size.x) > screen.SCREEN_WIDTH) {
-            self.speed.x *= -1;
-        }
+    pub fn reset(self: *Ball) void {
+        self.position = r.Vector2{ .x = 50, .y = 50 };
     }
 
-    pub fn paddle_collision_detection(self: *Ball, paddle: *p.Paddle) void {
+    pub fn tb_wall_collision_detection(self: *Ball) bool {
+        //check for y collisions
+        return self.position.y <= 0 or (self.position.y + self.size.y) > screen.SCREEN_HEIGHT;
+    }
+
+    pub fn lr_wall_collision_dectection(self: *Ball) bool {
+        // check for x collisions
+        return self.position.x <= 0 or (self.position.x + self.size.x) > screen.SCREEN_WIDTH;
+    }
+
+    pub fn paddle_collision_detection(self: *Ball, paddle: *p.Paddle) bool {
         // if ((self.position.y + self.size.y) >= paddle.position.y and (paddle.position.y + paddle.size.y) >= self.position.y) {
         //     self.speed.y *= -1;
         // }
-        if (((self.position.x + self.size.x) >= paddle.position.x and (self.position.y + self.size.y) >= paddle.position.y) and ((paddle.position.x + paddle.size.x) >= self.position.x and (paddle.position.y + paddle.size.y) >= self.position.y)) {
-            self.speed.x *= -1;
-        }
+        return ((self.position.x + self.size.x) >= paddle.position.x and (self.position.y + self.size.y) >= paddle.position.y) and ((paddle.position.x + paddle.size.x) >= self.position.x and (paddle.position.y + paddle.size.y) >= self.position.y);
     }
 
     pub fn draw(self: *Ball) void {
